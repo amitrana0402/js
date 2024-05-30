599aea7679d9da78fae0edab4ea113598ba1ba1d
amitrana0402 <42142428+amitrana0402@users.noreply.github.com>
2024-05-30 17:13:49 +0000

    conversion operation

---
+++ a/conversionOp.js
@@ -0,0 +1,60 @@
+let score = 'false'
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber);
+
+//"22"=>22
+// "33asv"=> NaN
+// true =>1
+
+let isLoggedIn ="sdds"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//1->true 0-> false
+//""-> false
+//"sdsd"-> true
+
+
+let someNumber = 22
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+// Operations
+
+let value =3
+let negValue = -value
+console.log(negValue);
+
+let str ="a"
+let str2 = " b"
+
+// let str3 = str+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+
+// console.log("1"+2+2)
+// console.log(1+2+"2");
+
+// console.log((3+4)*5);
+
+// console.log(+true)
+// console.log(+"");
+
+// let num1,num2,num3
+
+let gameCounter =100
+gameCounter++
+console.log(gameCounter)
