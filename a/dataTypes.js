cf67f130e140043237699d26d68d73aa88003417
amitrana0402 <42142428+amitrana0402@users.noreply.github.com>
2024-05-30 16:49:51 +0000

    data types

---
+++ a/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";
+// alert(3+3)
+// we are using node js not browser
+console.log(3+3) 
+// code readability shoud be high
+console.log("amit")
+let name="dssd"
+let age = 18
+let isLoggedIn =false 
+
+//number 
+// bigint
+//string
+//boolean
+//null standalone value
+// undefined => value not assigned
+// symbol => uniwue
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ a/variables.js
@@ -0,0 +1,14 @@
+const accountId =123242
+let accountEmail="fdfdf@dvfd.fdd"
+var accountPassword ="123244"
+accountCity = "delhi"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "sddssdsd@dd.dd"
+accountPassword="11111"
+accountCity ="chd"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
